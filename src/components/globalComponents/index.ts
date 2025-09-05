084d8f54aab9b0a2ff95dad3e9ac41df9ac62daa
Napat Srisamut <napatsrisamut1456@gmail.com>
2025-09-05 21:30:10 +0700

    add: button,input nostyle

--- src/components/globalComponents/index.ts
+++ src/components/globalComponents/index.ts
@@ -7,9 +7,12 @@ export {
 } from "./DefaultLayout";
 
 // Global UI Components
-export { ButtonExamples,GlobalButton } from "./component-ex";
+// export { ButtonExamples, GlobalButton } from "./component-ex";
 export { Footer } from "./Footer";
 export { Header } from "./Header";
+export { GlobalButton } from "./Button";
+export { GlobalInput } from "./Input";
+export { GlobalSearchInput } from "./SearchInput";
 
 // Re-export default
 export { default as Layout } from "./DefaultLayout";
