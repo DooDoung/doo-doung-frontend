a2ed1a90641cc9f959821f0cf496aab7d72686ff
Napat Srisamut <napatsrisamut1456@gmail.com>
2025-09-15 21:20:09 +0700

    add: fetch data from backend

--- src/pages/account/prophet/report.tsx
+++ src/pages/account/prophet/report.tsx
@@ -4,22 +4,46 @@ import ReportItem from "@/components/account/Report/ReportItem";
 import WarningBanner from "@/components/account/Report/WarningBanner";
 import UserProfile from "@/components/account/UserProfile";
 import { DefaultLayout } from "@/components/globalComponents";
-import {
-  mockReports,
-  type Report,
-  WARNING_THRESHOLD,
-} from "@/constants/reportConstants";
+import { type Report, WARNING_THRESHOLD } from "@/constants/reportConstants";
 
 export default function ProphetReportPage() {
   const [reports, setReports] = useState<Report[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
+  const fetchReportById = async (id: string) => {
+    const res = await fetch(`http://localhost:8000/report/${id}`);
+    const json = await res.json();
+    if (!res.ok) throw new Error("Failed to fetch report");
+    return json.data as Report;
+  };
+
   useEffect(() => {
     setIsLoading(true);
-    const timer = setTimeout(() => {
-      setReports(mockReports);
+
+    const fetchReports = async () => {
+      const mockIds = [
+        "11328be415f34f0a",
+        "56e41cb48a29444b",
+        "d1f102a78785433c",
+        "aca2f4765ac6443d",
+        "87b9ed110f1f4c55",
+        "92385d22fc0640bd",
+      ];
+      const fetchedReports: Report[] = [];
+
+      for (const id of mockIds) {
+        const report = await fetchReportById(id);
+        fetchedReports.push(report);
+      }
+
+      setReports(fetchedReports);
       setIsLoading(false);
+    };
+
+    const timer = setTimeout(() => {
+      fetchReports();
     }, 1000);
+
     return () => clearTimeout(timer);
   }, []);
 
