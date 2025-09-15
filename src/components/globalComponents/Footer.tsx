4088572cae5b98714dd094a421fe045386d9f3b0
Brnn043 <raksakul2442@gmail.com>
2025-09-15 21:47:23 +0700

    fix: styling

--- src/components/globalComponents/Footer.tsx
+++ src/components/globalComponents/Footer.tsx
@@ -8,18 +8,18 @@ export function Footer({ className }: FooterProps) {
   const currentYear = new Date().getFullYear();
 
   const contributors = [
-    "Akkharaphon Chowatthakawanit", "Pitchaya Arkahvipat",
-    "Chonlanan Srisanan", "Raksakul Hiranos",
-    "Napat Srisamut", "Sivikorn Nokham",
-    "Naphat Senirak", "Thananan Palanond",
-    "Nattarin Chetpatananondh", "Wanattha Chaturarat",
+    "Akkharaphon Chowatthakawanit", "Pitchaya Arkatvipat",
+    "Chonlanan Srisanan", "Raksakul Hiranas",
+    "Napat Srisamut", "Sivikom Nokham",
+    "Naphat Serirak", "Thananan Palanand",
+    "Nattarin Chetpattananondh", "Wanatha Chaturarat",
   ];
 
   return (
     <footer className={cn("text-white font-chakra", className)}>
       {/* ===== Upper Section ===== */}
       <div className="bg-neutral-white py-12 px-2 sm:px-8">
-        <div className="flex flex-col justify-between gap-8 md:flex-row container mx-auto max-w-screen-xl">
+        <div className="flex flex-col justify-between gap-8 md:flex-row container mx-12 max-w-screen-xl">
           
           {/* Col 1: Logo & Tagline */}
           <div className="flex flex-col gap-6 text-center md:text-left mt-8">
@@ -48,7 +48,7 @@ export function Footer({ className }: FooterProps) {
             <ul className="grid grid-cols-1 gap-x-8 gap-y-2 text-sm text-neutral-black sm:grid-cols-2">
               {contributors.map((name) => (
                 <li key={name} className="flex items-start">
-                  <span className="mr-2 mt-1">•</span>
+                  <span className="mr-2">•</span>
                   <span>{name}</span>
                 </li>
               ))}
@@ -60,7 +60,7 @@ export function Footer({ className }: FooterProps) {
 
       {/* ===== Bottom Section ===== */}
       <div className="bg-linear-to-r from-accent-pink to-accent-violet py-4">
-        <div className="container mx-auto max-w-screen-xl px-4 sm:px-8">
+        <div className="container mx-12 max-w-screen-xl px-4 sm:px-8">
           <p className="text-sm text-white">
             DooDoung @{currentYear} all rights reserved
           </p>
