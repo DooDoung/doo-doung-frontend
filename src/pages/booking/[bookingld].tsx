a33ec48f1858b0fc006ee3d313b716651a464c1f
chonlaprim <primsrisanan@gmail.com>
2025-10-19 10:01:14 +0700

    style: add Doodoung header and set gap

--- src/pages/booking/[bookingld].tsx
+++ src/pages/booking/[bookingld].tsx
@@ -3,7 +3,10 @@ import SessionTableBooking from "@/components/session/SessionTableBooking";
 
 export default function BookingPage() {
   return (
-    <DefaultLayout>
+    <DefaultLayout contentClassName="flex flex-col justify-center items-center">
+      <h1 className="font-sanctuary text-neutral-white text-center text-8xl mb-6 mt-6">
+        DooDoung
+      </h1>
       <div className="flex w-full flex-col items-center justify-center">
         <div></div>
         <div className="w-8/10 items-center self-center">
