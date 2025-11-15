c9517806ebf60a26cb9a4ea16fd71e44d0c08f7b
chonlaprim <primsrisanan@gmail.com>
2025-11-15 23:47:02 +0700

    fix: add reportStatus to ReportCard and update report fetching logic

--- src/components/account/Report/ReportCard.tsx
+++ src/components/account/Report/ReportCard.tsx
@@ -5,6 +5,7 @@ export type ReportCardProps = {
   imageUrl: string;
   title: string;
   description: string;
+  reportStatus: string;
   issueType: string;
 };
 
@@ -12,12 +13,27 @@ export default function ReportCard({
   imageUrl,
   title,
   description,
+  reportStatus,
   issueType,
 }: ReportCardProps) {
+
+  const setStyleStatus = () => {
+    switch(reportStatus){
+      case "PENDING": 
+        return "shadow-[0_0_6px_rgba(245,158,11,0.7)] border-2 border-warning text-warning";
+      case "DONE":
+        return "shadow-[0_0_6px_rgba(34,197,94,0.7)] border-2 border-success text-success";
+      case "DISCARD":
+        return "shadow-[0_0_6px_rgba(239,68,68,0.7)] border-2 border-error text-error";
+      default:
+        return "shadow-[0_0_6px_rgba(0,0,0,0.7)] border-2 border-neutral-black";
+    }
+  };
+
   return (
     <div className="bg-neutral-white flex w-[90%] items-center gap-4 rounded-2xl shadow-md backdrop-blur-sm transition-shadow hover:shadow-lg">
       {/* Book/Card Image */}
-      <div className="relative h-32 w-48 flex-shrink-0 overflow-hidden rounded-2xl">
+      <div className="relative h-32 w-48 shrink-0 overflow-hidden rounded-2xl">
         <Image src={imageUrl} alt={title} fill className="object-cover" />
       </div>
 
@@ -32,8 +48,11 @@ export default function ReportCard({
       </div>
 
       {/* Issue Type Badge */}
-      <div className="flex-shrink-0 px-4">
-        <span className="from-accent-pink to-accent-violet inline-flex rounded-full bg-gradient-to-r p-[2px]">
+      <div className="shrink-0 flex flex-col px-4 gap-4">
+        <span className={`bg-white text-center stroke-1 rounded-full text-base ${setStyleStatus()}`}>
+          {reportStatus}
+        </span>
+        <span className="from-accent-pink to-accent-violet inline-flex rounded-full bg-linear-to-r p-0.5">
           <span className="bg-neutral-white text-neutral-black inline-flex items-center rounded-full px-3 py-1 text-xs font-medium">
             {issueType.replaceAll("_", " ")}
           </span>
