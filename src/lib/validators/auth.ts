b3ff977947aba47e35cc0b79883dca3cf863c873
chera02 <thananan.it@topgunthailand.com>
2025-11-15 17:37:43 +0700

    fix: build

--- src/lib/validators/auth.ts
+++ src/lib/validators/auth.ts
@@ -3,6 +3,14 @@ import { z } from "zod";
 import { Sex, ZodiacSign } from "@/types/user";
 
 export const baseSchema = z.object({
+  firstName: z
+    .string()
+    .min(1, "First name is required")
+    .max(50, "First name must not exceed 50 characters"),
+  lastName: z
+    .string()
+    .min(1, "Last name is required")
+    .max(50, "Last name must not exceed 50 characters"),
   email: z
     .string()
     .email("Invalid email address")
